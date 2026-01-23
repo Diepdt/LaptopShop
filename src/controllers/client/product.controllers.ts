b454e76f386f1cf9aebc096d22d7589e29a3a753
Diepdt <duongtuandiep2@gmail.com>
2026-01-23 09:23:27 +0700

    #101 -Client xem chi tiết product

--- src/controllers/client/product.controllers.ts
+++ src/controllers/client/product.controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import { getProductInfo } from "../../services/admin/product.service";
 
-export const getClientProductPage = (req: Request, res: Response) => {
-    const id = req.params;
-    res.render("client/product/show.ejs", { id });
+export const getClientProductPage = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const product = await getProductInfo(id);
+    res.render("client/product/show.ejs", { product });
 }
